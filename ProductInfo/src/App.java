8b3d3d2e351dfd3d78299e6b543520c9dc74921e
AliyevaVasila <vasilaaliyeffa@gmail.com>
2023-07-07 18:41:08 +0300

    check

--- ProductInfo/src/App.java
+++ ProductInfo/src/App.java
@@ -1,4 +1,20 @@
-public class App {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+
+public class App extends Application implements EventHandler<ActionEvent> {
     public static void main(String[] args) throws Exception {
+
+    }
+
+    @Override
+    public void handle(ActionEvent arg0) {
+
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
     }
 }
