1c8693346ada53dc8e14b000014be41b548b77ef
Rahima_Karimova <rkarimova17347@ada.edu.az>
2023-08-14 19:39:31 +0400

    adding

--- ProductInfo/src/App.java
+++ ProductInfo/src/App.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +21,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -28,6 +32,7 @@ public class App extends Application {
     Button button1, button2, button3;
 
     private List<Customer> customers = new ArrayList<>();
+    private int selectedIndex = -1;
 
     public static void main(String[] args) {
         launch(args);
@@ -53,7 +58,7 @@ public class App extends Application {
         TableColumn<Customer, Character> genColumn = new TableColumn<>("Gender");
         TableColumn<Customer, Integer> disColumn = new TableColumn<>("Discount");
 
-        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("ID"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         sNameColumn.setCellValueFactory(new PropertyValueFactory<>("surname"));
         genColumn.setCellValueFactory(new PropertyValueFactory<>("gender"));
@@ -70,7 +75,7 @@ public class App extends Application {
         tableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Customer>() {
             @Override
             public void changed(ObservableValue<? extends Customer> observable, Customer oldValue, Customer newValue) {
-                int selectedIndex = tableView.getSelectionModel().getSelectedIndex();
+                selectedIndex = tableView.getSelectionModel().getSelectedIndex();
                 button2.setDisable(selectedIndex < 0);
                 button3.setDisable(selectedIndex < 0);
             }
@@ -89,7 +94,7 @@ public class App extends Application {
         rootPane.setTop(hbox1);
         rootPane.setCenter(tableView);
 
-        Scene scene = new Scene(rootPane, 620, 360);
+        Scene scene = new Scene(rootPane);
         stage.setScene(scene);
 
         stage.show();
@@ -113,18 +118,32 @@ public class App extends Application {
         }
     }
 
-    private void addCustomer(String name, String surname, char gender, int discount) {
-        int id = customers.size() + 1;
-        Customer newCustomer = new Customer(name, surname, gender, discount);
-        customers.add(newCustomer);
+    private void addCustomer(Customer customer) {
+        customers.add(customer);
     }
 
-    private void updateCustomer(int index, String name, String surname, char gender, int discount) {
+    private void updateCustomer(int index, String name, String surname, Character gender, Integer discount) {
         Customer customer = customers.get(index);
+
         customer.setName(name);
+
         customer.setSurname(surname);
+
         customer.setGender(gender);
+
         customer.setDiscount(discount);
+
+    }
+
+    private void saveToCSV(Customer customer) {
+        String data = customer.getID() + "," + customer.getName() + "," + customer.getSurname() + ","
+                + customer.getGender() + "," + customer.getDiscount();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data.csv", true))) {
+            writer.write(data);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void deleteCustomer(int index) {
@@ -145,24 +164,30 @@ public class App extends Application {
         TextField field4 = new TextField();
         TextField field5 = new TextField();
 
-        VBox vbox = new VBox(10);
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setPadding(new Insets(20));
-        vbox.getChildren().addAll(label2, label3, label4, label5);
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(20));
+        gridPane.add(label2, 0, 0);
+        gridPane.add(label3, 0, 1);
+        gridPane.add(label4, 0, 2);
+        gridPane.add(label5, 0, 3);
+        gridPane.add(field2, 1, 0);
+        gridPane.add(field3, 1, 1);
+        gridPane.add(field4, 1, 2);
+        gridPane.add(field5, 1, 3);
 
         Button button4 = new Button("Save");
 
         VBox vbox2 = new VBox(10);
         vbox2.setAlignment(Pos.CENTER);
-        vbox2.setPadding(new Insets(20));
 
-        vbox2.getChildren().addAll(field2, field3, field4, field5, button4);
+        vbox2.getChildren().add(button4);
 
-        HBox hBox2 = new HBox(10);
-        hBox2.setAlignment(Pos.CENTER);
-        hBox2.getChildren().addAll(vbox, vbox2);
+        gridPane.add(vbox2, 1, 4);
 
-        Scene scene = new Scene(hBox2, 360, 180);
+        Scene scene = new Scene(gridPane, 360, 180);
         newStage.setScene(scene);
 
         // Handle the Save button click event
@@ -171,11 +196,16 @@ public class App extends Application {
             String name = field2.getText();
             String surname = field3.getText();
             char gender = field4.getText().charAt(0);
-            int discount = Integer.parseInt(field5.getText());
-
+            int discount;
+            try {
+                discount = Integer.parseInt(field5.getText());
+            } catch (NumberFormatException ex) {
+                discount = 0; // Default value or handle the error case
+            }
+            Customer newCustomer = new Customer(name, surname, gender, discount);
             if (selectedIndex == -1) {
                 // Create a new customer
-                addCustomer(name, surname, gender, discount);
+                addCustomer(newCustomer);
             } else {
                 // Update an existing customer
                 updateCustomer(selectedIndex, name, surname, gender, discount);
@@ -183,12 +213,11 @@ public class App extends Application {
 
             // Update the TableView
             tableView.setItems(FXCollections.observableArrayList(customers));
+            saveToCSV(newCustomer);
+
             newStage.close();
         });
 
         newStage.show();
     }
-
-   
 }
-
